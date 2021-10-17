de3a79c13d2fa62264e4da96991ff5e5f36c6f4a
Leesiw <84753357+Leesiw@users.noreply.github.com>
2021-10-17 21:29:28 +0900

    테스트

--- mario resource/mariotest.py
+++ mario resource/mariotest.py
@@ -1,84 +1,102 @@
 from pico2d import *
 from character import *
 from map import *
+from interaction import *
 
+def draw_character():
+    draw(mario, camera)
+    for i in range(test_map.monster_number['goomba']):
+        if all_goomba[i].die == 'alive':
+            monster_move(all_goomba[i])
+            draw(all_goomba[i], camera)
+    for i in range(test_map.monster_number['koopagreen']):
+        if all_koopagreen[i].die == 'alive':
+            monster_move(all_koopagreen[i])
+            draw(all_koopagreen[i], camera)
 
-def change_state(character, state):
-    character.state_move = state
-    character.ani[mario.state_move].frame_now = 0
+def change_animation(character, ani_name):
+    character.animation = ani_name
+    character.ani[mario.animation].frame_now = 0
 
-
-def move(character):
-    global frame
-    character.x += character.speed
-    if character.state_move == 'left_jump':
-        if frame == character.ani['left_jump'].frame - 1:
-            if character.speed == 0:
-                change_state(character, 'left_standing')
-            else:
-                change_state(character, 'left_run')
-    elif character.state_move == 'right_jump':
-        if frame == character.ani['right_jump'].frame - 1:
-            if character.speed == 0:
-                change_state(character, 'right_standing')
-            else:
-                change_state(character, 'right_run')
+def restart():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == 'r':
+                mario.die = 'small'
+                mario.animation = 'right_run'
 
 def handle_events():
     global running
-    global mario
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                mario.speed += 5
-                change_state(mario, 'right_run')
+                mario.state = 'right'
+                change_animation(mario, 'right_run')
             elif event.key == SDLK_LEFT:
-                mario.speed -= 5
-                change_state(mario, 'left_run')
+                mario.state = 'left'
+                change_animation(mario, 'left_run')
             if event.key == SDLK_SPACE:
-                if mario.speed > 0:
-                    change_state(mario, 'right_jump')
-                elif mario.speed < 0:
-                    change_state(mario, 'left_jump')
-                else:
-                    if mario.state_move == 'right_standing':
-                        change_state(mario, 'right_jump')
+                if not mario.jump_bool:
+                    mario.jump_bool = True
+                    mario.jump_power = 17
+                    if mario.speed > 0:
+                        change_animation(mario, 'right_jump')
+                    elif mario.speed < 0:
+                        change_animation(mario, 'left_jump')
                     else:
-                        change_state(mario, 'left_jump')
+                        if mario.animation == 'right_standing':
+                            change_animation(mario, 'right_jump')
+                        else:
+                            change_animation(mario, 'left_jump')
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
-                mario.speed -= 5
-                change_state(mario, 'right_standing')
+                mario.state = 'standing'
+                mario.speed = 0
+                change_animation(mario, 'right_standing')
             elif event.key == SDLK_LEFT:
-                mario.speed += 5
-                change_state(mario, 'left_standing')
+                mario.state = 'standing'
+                mario.speed = 0
+                # mario.speed += 10
+                change_animation(mario, 'left_standing')
 
 
 open_canvas()
 
 bk_ground = load_image('bg.png')
 mario_image = load_image('mario.png.gif')
-
-
+game_over = load_image('gameover.png')
+mario.small_big = 'small'
 
 running = True
 speed = 0
 frame = 0
+die = False
 
 while running:
-    bk_ground.draw(400, 300, 800, 600)
-    draw_map(test_map, test_map.tile_name)
-    mario_image.clip_draw(mario.ani[mario.state_move].start_x + mario.ani[mario.state_move].frame_now * mario.ani[mario.state_move].next,
-                          mario.ani[mario.state_move].start_y,
-                          mario.ani[mario.state_move].width, mario.ani[mario.state_move].height,
-                          mario.x, mario.y, mario.size_x, mario.size_y)
-    mario.ani[mario.state_move].frame_now = (mario.ani[mario.state_move].frame_now + 1) % mario.ani[mario.state_move].frame
-    update_canvas()
-    handle_events()
-    move(mario)
-    delay(0.05)
+    if mario.die != 'die':
+        bk_ground.draw(400, 300, 800, 600)
+        update_camera(camera, test_map, mario)
+        draw_map(test_map, camera)
+        if mario.die != 'die_ani':
+            mario_with_monster(mario, all_goomba, test_map)
+            mario_with_monster(mario, all_koopagreen, test_map)
+
+        draw_character()
+
+        update_canvas()
+        handle_events()
+        move(mario)
+        delay(0.005)
+    else:
+        game_over.draw(400, 300, 800, 600)
+        restart()
+        update_canvas()
 
 close_canvas()
