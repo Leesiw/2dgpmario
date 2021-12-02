83839ab15667fdcbefb6e21de471211e37da95d4
Leesiw <84753357+Leesiw@users.noreply.github.com>
2021-12-02 22:07:41 +0900

    ui 추가

---
+++ ui.py
@@ -0,0 +1,22 @@
+from pico2d import *
+import game_framework
+
+class Ui:
+    font = None
+    def __init__(self):
+        self.score = 0
+        self.time = 100
+        self.life_num = 3
+
+        if Ui.font == None:
+            Ui.font = load_font('resource/Super Mario Bros.ttf', 30)
+
+
+    def draw(self, camera_x, camera_y):
+        self.font.draw(0, 550, 'score : %d' % (self.score), (255, 255, 255))
+        self.font.draw(0, 500, 'life : %d' % (self.life_num), (0, 0, 0))
+        self.font.draw(600, 550, 'time : %d' % (self.time), (255, 255, 255))
+
+
+    def update(self):
+        self.time -= game_framework.frame_time
