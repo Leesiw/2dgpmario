ff8bd3b762aba9dcac11e6c89835d0029f7c08c8
Leesiw <84753357+Leesiw@users.noreply.github.com>
2021-12-07 17:45:28 +0900

    ui 수정

--- ui.py
+++ ui.py
@@ -6,10 +6,11 @@ import server
 class Ui:
     font = None
     Big_font = None
-    def __init__(self):
+    def __init__(self, full_score):
         self.score = 0
         self.time = 100
         self.life_num = 3
+        self.full_score = full_score
 
         if Ui.font == None:
             Ui.font = load_font('resource/Super Mario Bros.ttf', 30)
@@ -19,7 +20,7 @@ class Ui:
 
 
     def draw(self, camera_x, camera_y):
-        self.font.draw(0, 550, 'score : %d' % (self.score), (255, 255, 255))
+        self.font.draw(0, 550, 'score : %d / %d' % (self.score, self.full_score), (255, 255, 255))
         self.font.draw(0, 500, 'life : %d' % (self.life_num), (0, 0, 0))
         self.font.draw(600, 550, 'time : %d' % (self.time), (255, 255, 255))
 
